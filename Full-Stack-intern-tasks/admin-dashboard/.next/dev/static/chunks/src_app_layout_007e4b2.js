2b6802eed31562d8882a1aa2ebd605e2522c2ebf
Pawan kumar <pawankr16123114@gmail.com>
2026-06-20 11:59:27 +0530

    Final cleanup

--- Full-Stack-intern-tasks/admin-dashboard/.next/dev/static/chunks/src_app_layout_007e4b2.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__1nlndi9._.css","static/chunks/node_modules_06qfvou._.js","static/chunks/src_context_1u3_8yg._.js"],
-    source: "dynamic"
-});
--- Full-Stack-intern-tasks/admin-dashboard/.next/dev/static/chunks/src_app_page_10b-syc.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_1qdxaxd._.js","static/chunks/node_modules_1p5d88p._.js"],
-    source: "dynamic"
-});
